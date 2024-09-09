28852b9db0085bf8436a47a5e0215fdf4f9c0607
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-09 15:05:02 -0700

    And this is interesting

--- custom_components/multi_zone_receiver/config_flow.py
+++ custom_components/multi_zone_receiver/config_flow.py
@@ -3,10 +3,36 @@
 import asyncio
 
 from homeassistant import config_entries
+from homeassistant.components.media_player import DOMAIN as MEDIA_PLAYER_DOMAIN
+from homeassistant.const import CONF_ICON, CONF_NAME, CONF_SOURCE
 from homeassistant.core import callback
+from homeassistant.helpers import config_validation as cv
 import voluptuous as vol
 
-from .const import CONF_PASSWORD, CONF_USERNAME, DOMAIN, PLATFORMS
+from .const import CONF_PASSWORD, CONF_USERNAME, CONF_ZONES, DOMAIN, PLATFORMS
+
+# This is the schema that used to display the UI to the user. This simple
+# schema has a single required host field, but it could include a number of fields
+# such as username, password etc. See other components in the HA core code for
+# further examples.
+# Note the input displayed to the user will be translated. See the
+# translations/<lang>.json file and strings.json. See here for further information:
+# https://developers.home-assistant.io/docs/config_entries_config_flow_handler/#translations
+# At the time of writing I found the translations created by the scaffold didn't
+# quite work as documented and always gave me the "Lokalise key references" string
+# (in square brackets), rather than the actual translated value. I did not attempt to
+# figure this out or look further into it.
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_ZONES): cv.ensure_list(
+            {
+                vol.Required(CONF_SOURCE): cv.entity_domain(MEDIA_PLAYER_DOMAIN),
+                vol.Optional(CONF_NAME): cv.string,
+                vol.Optional(CONF_ICON): cv.icon,
+            }
+        )
+    }
+)
 
 
 class MultiZoneReceiverFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
@@ -51,9 +77,7 @@ class MultiZoneReceiverFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         """Show the configuration form to edit location data."""
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {vol.Required(CONF_USERNAME): str, vol.Required(CONF_PASSWORD): str}
-            ),
+            data_schema=DATA_SCHEMA,
             errors=self._errors,
         )
 
