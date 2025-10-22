b992e869c3aba57c5d579fa4cf440e7731b5c280
Jordan Zucker <jordan.zucker@gmail.com>
2025-10-22 14:01:05 -0700

    And fixed

--- custom_components/multi_zone_receiver/__init__.py
+++ custom_components/multi_zone_receiver/__init__.py
@@ -10,7 +10,8 @@ import logging
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_NAME
-from homeassistant.core import Config, HomeAssistant
+from homeassistant.core import HomeAssistant
+from homeassistant.core_config import Config
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.reload import async_setup_reload_service
 
